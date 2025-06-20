34e2de42adb3ec90294dc53be6ecd2427688680d
Faiz <xfaiz45@gmail.com>
2025-06-20 16:40:29 +0530

    Updated

--- app.py
+++ app.py
@@ -69,7 +69,8 @@ def handle_reaction(data):
     emit('receive-reaction', data, broadcast=True, include_self=False)
 
 
-# ---- Run Server ----
+import os
+
 if __name__ == '__main__':
-    print("Server running at http://localhost:5050")
-    socketio.run(app, debug=True, port=5050)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
